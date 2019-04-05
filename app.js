92dcd69fd368a86e7ef3d2ff2000393fbb660a15
saifk786 <zainkhan.srk1998@gmail.com>
2019-04-05 12:40:36 +0530

    heloooo

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var hbs = require('hbs');
+var fs = require('fs');
+var app = express();
+hbs.registerPartials(__dirname+'/views/partials')
+app.set('view engine','hbs');
+
+app.get('/',(req,res)=>{
+    res.render('books.hbs',{
+        heading: "Welcome to my Website",
+
+    });
+});
+app.get('/books',(req,res)=>{
+    res.render('books.hbs',{
+        Heading:'maths',
+        product1: "5 units",
+        desc: "schand"
+    });
+});
+app.get('/electronics',(req,res)=>{
+    res.render('electronics.hbs',{
+        Heading:'320 laptop',
+        product1: "10 units",
+        desc: "lenovo"
+    });
+});
+app.get('/clothes',(req,res)=>{
+    res.render('clothes.hbs',{
+        Heading:'Shirts ',
+        product1: "4 units",
+        desc: "Reebok"
+    });
+});
+
+
+
+
+app.listen(3000,() => {console.log('server started at port 3000')});
+
+
